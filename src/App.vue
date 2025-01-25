11910727d6bfce04dd1719472569f832c7e99791
天才熊猫君 <1799385140@qq.com>
2025-01-25 16:05:36 +0800

    初始化了各种插件，比如eslint插件，pinia插件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <script setup>
+import { useUserStore } from './stores/index.js'
+
+const userStore = useUserStore()
+const a = ref(0)
 </script>
 
 <template>
-  我是APP
+  我是APP{{ a }}----{{ userStore.token }}
+
+  <el-button type="success" @click="userStore.setToken('123')">
+    Success
+  </el-button>
 </template>
 
 <style scoped>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
@@ -10,11 +9,17 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [vue(), vueDevTools(), AutoImport({
-    resolvers: [ElementPlusResolver()],
-  }), Components({
-    resolvers: [ElementPlusResolver()],
-  })],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    AutoImport({ // 自动导入 vue3 的api 和 element-plus 的api
+      imports: ['vue', 'vue-router'],
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({ // 自动打包使用的 element-plus 的组件
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
